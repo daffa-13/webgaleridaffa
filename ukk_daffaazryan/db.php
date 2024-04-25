76f3154bc941dee07aee2d39a97316d9e28bc063
daffa-13 <168047020+daffa-13@users.noreply.github.com>
2024-04-25 09:44:58 +0700

    Add files via upload

---
+++ ukk_daffaazryan/db.php
@@ -0,0 +1,8 @@
+<?php
+    $hostname = 'localhost';
+	$username = 'root';
+	$password = '';
+	$dbname   = 'ukk2024_daffaazryannfadhlurrohman';
+	
+	$conn = mysqli_connect($hostname, $username, $password, $dbname) or die ('gagal terhubung ke database');
+?>
